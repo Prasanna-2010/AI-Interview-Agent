11ed46f091f53a75bd97553f9fd46c8965e1ed9e
Prasanna <prasannaamin20@gmail.com>
2026-08-19 07:01:17 +0530

    Updtaed Model

--- backend/llm.py
+++ backend/llm.py
@@ -28,7 +28,7 @@ def generate_response(messages):
     """
 
     response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
+        model="openai/gpt-oss-20b",
         messages=messages,
         temperature=0.3,
         max_tokens=500
